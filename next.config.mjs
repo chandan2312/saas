98e91b2b5161a05c20eb93f7ea39a922aaf683a6
chandan2312 <chandanchaudhari2312@gmail.com>
2024-01-31 01:32:49 +0530

    build:setup & routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: false,
+	images: {
+		domains: [
+			"images.unsplash.com",
+			"res.cloudinary.com",
+			"files.stripe.com",
+			"uploadthing.com",
+			"utfs.com",
+			"img.clerk.com",
+			"subdomain",
+		],
+	},
+};
 
 export default nextConfig;
